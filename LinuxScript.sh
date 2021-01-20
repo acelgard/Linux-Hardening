8a4748ac70fa0105b5188bae5015e7226fdc35c5
acelgard <45463851+acelgard@users.noreply.github.com>
2021-01-20 14:02:30 -0600

    Update LinuxScript.sh
    systemctl added

--- LinuxScript.sh
+++ LinuxScript.sh
@@ -1,14 +1,14 @@
 #1/14/2021
 #script to harden Linux operating systems
-echo "Begining harding script ..."
+echo "Begining hardening script ..."
 @echo off
 sudo yum install aide tmux iptables
 sudo yum install iptables-services -y
 
-#systemctl stop firewalld
-#systemctl disable firewalld
-#systemctl start iptables
-#systemctl status iptables
+systemctl stop firewalld
+systemctl disable firewalld
+systemctl start iptables
+systemctl status iptables
 
 
 rm /etc/issue
