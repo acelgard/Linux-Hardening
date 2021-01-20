9187668f066ec177113d6b6e5c5073767f8261c7
mark <mark@localhost.localdomain>
2021-01-20 13:57:03 -0600

    adding a banner before log on

--- LinuxScript.sh
+++ LinuxScript.sh
@@ -5,9 +5,14 @@ echo "Begining harding script ..."
 sudo yum install aide tmux iptables
 sudo yum install iptables-services -y
 
-systemctl stop firewalld
-systemctl disable firewalld
-systemctl start iptables
-systemctl status iptables
+#systemctl stop firewalld
+#systemctl disable firewalld
+#systemctl start iptables
+#systemctl status iptables
 
-echo "End of script"
+
+rm /etc/issue
+echo -e "WARNING! Access to this device is restricted to those individuals with specific
+Permissions. If you are not an authorized user, disconnect now.
+Any attempts to gain unauthorized access will be prosecuted to
+the fullest extent of the law" >> /etc/issue
