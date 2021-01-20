f415e287479fa17a3250b44177f1ec7623a0e20d
mark <mark@localhost.localdomain>
2021-01-20 14:10:23 -0600

    Renamed script

---
+++ fedoraScript.sh
@@ -0,0 +1,18 @@
+#1/14/2021
+#script to harden Linux operating systems
+echo "Begining hardening script ..."
+@echo off
+sudo yum install aide tmux iptables
+sudo yum install iptables-services -y
+
+systemctl stop firewalld
+systemctl disable firewalld
+systemctl start iptables
+systemctl status iptables
+
+
+rm /etc/issue
+echo -e "WARNING! Access to this device is restricted to those individuals with specific
+Permissions. If you are not an authorized user, disconnect now.
+Any attempts to gain unauthorized access will be prosecuted to
+the fullest extent of the law" >> /etc/issue
